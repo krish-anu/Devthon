b268f8fc17b94ea03ee6211ee857abb93bb7536d
Ruththra <ruththu2003@gmail.com>
2026-02-09 15:03:27 +0530

    feat: add email domain validation to passkey login DTOs and update profile DTO

--- server/src/auth/dto/passkey-login-finish.dto.ts
+++ server/src/auth/dto/passkey-login-finish.dto.ts
@@ -1,7 +1,9 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmailDomainDeliverable } from '../../common/validators/email-mx.validator';
 
 export class PasskeyLoginFinishDto {
   @IsEmail()
+  @IsEmailDomainDeliverable({ message: 'Email domain cannot receive mail' })
   email: string;
 
   @IsNotEmpty()
