eef6564a0acac1be48938e9ed5d5a2175ed289e9
krish-anu <krishnaanu200302@gmail.com>
2026-02-18 22:32:03 +0530

    refactor: improve GoogleMap props typing by omitting onClick and onLoad handlers

--- client/types/react-google-maps-api.d.ts
+++ client/types/react-google-maps-api.d.ts
@@ -4,11 +4,15 @@ declare module '@react-google-maps/api' {
 
   export function useLoadScript(options: { googleMapsApiKey: string; libraries?: string[]; }): { isLoaded: boolean; loadError?: Error };
 
-  export function GoogleMap(props: React.ComponentProps<'div'> & {
+  type DivPropsWithoutMapHandlers = Omit<React.ComponentProps<'div'>, 'onClick' | 'onLoad'>;
+
+  export function GoogleMap(props: DivPropsWithoutMapHandlers & {
     mapContainerStyle?: React.CSSProperties;
     center?: LatLngLiteral;
     zoom?: number;
+    options?: MapOptions;
     onClick?: (e: google.maps.MapMouseEvent) => void;
+    onLoad?: (map: google.maps.Map) => void;
   }): JSX.Element;
 
   export function Marker(props: { position: LatLngLiteral } & React.ComponentProps<'div'>): JSX.Element;
